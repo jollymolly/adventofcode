5e267f2b5fe2485aa1d6be97c47bf530edbf76b7
Oleksandr Saienko <sajenko@gmail.com>
2020-12-04 11:07:29 +0200

    Day 4: completed part 2.

--- 2020/day4/day4.py
+++ 2020/day4/day4.py
@@ -4,10 +4,20 @@ import sys
 
 _REQUIRED_FIELDS = frozenset(("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", ))
 
+_VALIDATORS = {
+    "byr": lambda v: len(v) == 4 and v.isnumeric() and 1920 < int(v) <= 2002,
+    "iyr": lambda v: len(v) == 4 and v.isnumeric() and 2010 <= int(v) <= 2020,
+    "eyr": lambda v: len(v) == 4 and v.isnumeric() and 2020 <= int(v) <= 2030,
+    "hgt": lambda v: v[-2:] in frozenset(("cm", "in", )) and (v[-2:] == "cm" and 150 <= int(v[:-2]) <= 193 or 59 <= int(v[:-2]) <= 76),
+    "hcl": lambda v: v[0] == '#' and len(v[1:]) == 6 and all(ch in frozenset("abcdef0123456789") for ch in v[1:]),
+    "ecl": lambda v: v in frozenset(("amb", "blu", "brn", "gry", "grn", "hzl", "oth")),
+    "pid": lambda v: v.isnumeric() and len(v) == 9
+}
+
 
 def _is_valid_passport_data(data):
-    return _REQUIRED_FIELDS - frozenset(data) == frozenset()
-
+    return _REQUIRED_FIELDS - frozenset(data) == frozenset() \
+        and all(_VALIDATORS[k](v.strip()) is True for k, v in data.items() if k in _VALIDATORS)
 
 def validate(filename: str):
 
